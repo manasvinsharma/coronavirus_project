1ca3ed0527eee72fd527207d1a0bce7a4dc0b153
KY <yadavkavita7865@gmail.com>
2020-06-19 22:42:58 +0530

    testCorona

---
+++ src1/app/corona-news/corona-news.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-corona-news',
+  templateUrl: './corona-news.component.html',
+  styleUrls: ['./corona-news.component.css']
+})
+export class CoronaNewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
